36b5ec0c6a612ecd62c1296a738a2c98ced5a316
yuriko192 <ivanwijaya19@yahoo.co.id>
2021-04-17 15:26:34 +0700

    added rankings page

---
+++ src/pages/Rankings.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex column PageRankings">
+    <navbar :type="'Profile'" />
+  </q-page>
+</template>
+
+<script>
+import Navbar from "src/components/Navbar.vue";
+export default {
+  components: { Navbar },
+  name: "Rankings",
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style>
