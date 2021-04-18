bf6fc8d9e5772e637f2c4d5a5e07f1636acf04dd
yuriko192 <ivanwijaya19@yahoo.co.id>
2021-04-19 00:37:59 +0700

    edited profile page

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,18 +1,39 @@
 <template>
   <q-page class="flex column PageProfile">
     <navbar />
-    <div class="ProfileInfo">
-      <img
-        :src="require('../assets/' + this.$store.state.Player.PicUrl)"
-        alt=""
-        class="PlayerImage"
-      />
-      <h5 class="Title">Ivan Wijaya</h5>
-      <p class="SubTitle">Salesman</p>
-    </div>
-    <div class="Dashboard">
-      <p class="Title">This Month Performance</p>
-      <p class="SubTitle">Let's See How Many Task You Completed</p>
+    <div class="Content">
+      <div class="Charts">
+        <div class="Description">
+          <h5 class="Title">Monthly Performance</h5>
+          <p class="SubTitle">Review your past performance to improve</p>
+        </div>
+        <div class="Performance">
+          <h5 class="ChartTitle">Februari 2021</h5>
+          <div class="Information">
+            <q-circular-progress
+              show-value
+              :value="74"
+              size="35vw"
+              :thickness="0.1"
+              color="blue"
+              center-color="transparent"
+              track-color="grey"
+              class="CircularChart"
+            >
+              <span class="ChartContent"> 76% </span>
+            </q-circular-progress>
+            <div class="Agreement">
+              <p class="Title">Total Agreement</p>
+              <div class="Value">5 <span class="Details">out of 8</span></div>
+            </div>
+            <div class="Reward">
+              <p class="Title">Total Reward</p>
+              <div class="Value">Rp. 1.000.000</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="History"></div>
     </div>
   </q-page>
 </template>
@@ -27,47 +48,76 @@ export default {
 
 
 <style lang="scss" scoped>
-.ProfileInfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  margin-top: 2rem;
-
-  .Title {
-    font-weight: bold;
-    margin: 2rem 0 0 0;
-  }
-  .SubTitle {
-    color: #f5ac1f;
-  }
+.Content {
+  padding: 0 1.5rem;
 }
-.Dashboard {
-  display: flex;
-  flex-direction: column;
-
-  margin: 1rem;
-  padding: 1.5rem;
-  border-radius: 25px;
-
-  background: rgba(255, 255, 255, 0.09)
-    linear-gradient(
-      250.02deg,
-      rgba(36, 0, 138, 0.1) 10.99%,
-      rgba(60, 60, 60, 0.1) 93.39%
-    );
-
-  text-align: center;
-  .Title {
-    font-size: 1.2rem;
-    font-weight: bold;
+.Charts {
+  .Description {
+    .Title {
+      font-weight: bold;
+    }
+    .SubTitle {
+      font-weight: bold;
+      color: #ff9393;
+    }
   }
-  .SubTitle {
-    font-weight: lighter;
+  .Performance {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 100%;
+
+    margin: 1rem 0;
+    padding: 1rem;
+    border-radius: 25px;
+
+    background: rgba(255, 255, 255, 0.09);
+
+    .ChartTitle {
+      font-size: 1.1rem;
+      font-weight: bold;
+    }
+    .Information {
+      margin-top: 1rem;
+      display: grid;
+      width: 100%;
+      grid-template-areas:
+        "chart agreement"
+        "chart reward";
+      .ChartContent {
+        font-size: 1.3rem;
+        font-weight: bold;
+      }
+      .CircularChart {
+        grid-area: chart;
+      }
+      .Agreement {
+        grid-area: agreement;
+        .Value {
+          color: #02dac5;
+          .Details {
+            color: #00a595;
+          }
+        }
+      }
+      .Reward {
+        grid-area: reward;
+        .Value {
+          color: #e5a01b;
+        }
+      }
+      .Title {
+        font-weight: bold;
+      }
+      .Value {
+        font-size: 1.2rem;
+        font-weight: bold;
+        .Details {
+          font-size: 0.8rem;
+        }
+      }
+    }
   }
-}
-.PlayerImage {
-  height: 180px;
-  border-radius: 180px;
 }
 </style>
