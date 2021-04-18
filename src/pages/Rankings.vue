fbbfb4e1de9582da018b91e903874b23e9c1cf34
yuriko192 <ivanwijaya19@yahoo.co.id>
2021-04-18 21:20:29 +0700

    rankings done

--- src/pages/Rankings.vue
+++ src/pages/Rankings.vue
@@ -1,6 +1,10 @@
 <template>
   <q-page class="flex column PageRankings">
     <navbar :type="'Profile'" />
+    <div class="Description">
+      <p class="SubTitle">See your rank beetween your collegues</p>
+      <h5 class="Title">Monthly Tier</h5>
+    </div>
     <div class="PlayerList">
       <Player-Card
         :PlayerName="'Carlene Lim'"
@@ -8,24 +12,28 @@
         :Level="1"
         :PlayerIcon="'Player (0).png'"
         :IsActive="true"
+        :Rank="1"
       />
       <Player-Card
         :PlayerName="'Ivan WIjaya'"
         :GameName="'CassieBeta'"
         :Level="2"
         :PlayerIcon="'Player (1).png'"
+        :Rank="2"
       />
       <Player-Card
         :PlayerName="'Mitchell Ryuu'"
         :GameName="'CassieCharlie'"
         :Level="3"
         :PlayerIcon="'Player (2).png'"
+        :Rank="3"
       />
       <Player-Card
         :PlayerName="'Wijaya Lim'"
         :GameName="'CassieDelta'"
         :Level="4"
         :PlayerIcon="'Player (3).png'"
+        :Rank="4"
       />
       <Player-Card
         :PlayerName="'Wijaya Lim'"
@@ -102,6 +110,15 @@ export default {
 
 
 <style lang="scss" scoped>
+.Description {
+  padding: 0.4rem 2rem;
+  .Title {
+    font-weight: bold;
+  }
+  .SubTitle {
+    color: #ff9393;
+  }
+}
 .PlayerList {
   //bikin component ini scrollable
   height: calc(100vh - 5rem);
