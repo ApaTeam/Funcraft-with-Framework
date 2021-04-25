85eedd750a57485e63d395959ab4c1a9fc77342b
yuriko192 <ivanwijaya19@yahoo.co.id>
2021-04-25 20:00:07 +0700

    added quest to index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -56,6 +56,58 @@
             </div>
           </div>
         </div>
+        <div class="QuestArea">
+          <div class="MonthlyQuest">
+            <h5 class="Title">Monthly Quest</h5>
+            <div class="QuestList">
+              <div class="QuestContainer" @click="chpage(0)">
+                <p class="QuestName">
+                  Make Agreement
+                  <span class="QuestRate">(5/7)</span>
+                </p>
+
+                <q-linear-progress
+                  dark
+                  :value="5 / 7"
+                  size=".5rem"
+                  class="ProgressBar"
+                  track-color="black"
+                />
+                <h5 class="QuestPoint">5 Pts</h5>
+              </div>
+            </div>
+          </div>
+          <div class="DailyQuest">
+            <h5 class="Title">Daily Quest</h5>
+            <q-scroll-area horizontal class="QuestList">
+              <div class="row no-wrap">
+                <div class="QuestContainer" @click="chpage(1)">
+                  <p class="QuestName">Make Agreement</p>
+                  <span class="QuestRate">(5/7)</span>
+                  <q-linear-progress
+                    :value="5 / 7"
+                    size=".5rem"
+                    class="ProgressBar"
+                    track-color="black"
+                  />
+                  <h5 class="QuestPoint">5 Pts</h5>
+                </div>
+                <div class="QuestContainer" @click="chpage(2)">
+                  <p class="QuestName">Make Agreement</p>
+                  <span class="QuestRate">(5/7)</span>
+
+                  <q-linear-progress
+                    :value="5 / 7"
+                    size=".5rem"
+                    class="ProgressBar"
+                    track-color="black"
+                  />
+                  <h5 class="QuestPoint">5 Pts</h5>
+                </div>
+              </div>
+            </q-scroll-area>
+          </div>
+        </div>
       </div>
     </div>
   </q-page>
@@ -99,6 +151,9 @@ export default {
           this.isTop = true;
         }
       }, 20);
+    },
+    chpage(QuestId) {
+      this.$router.push({ path: "/quest" });
     },
   },
 };
@@ -181,6 +236,104 @@ export default {
       }
       .StatValue {
         width: 30%;
+      }
+    }
+  }
+}
+.QuestArea {
+  display: flex;
+  flex-direction: column;
+  > * {
+    margin-bottom: 1rem;
+  }
+  .Title {
+    font-size: 1.2rem;
+    margin-bottom: 1rem;
+  }
+  .QuestContainer {
+    border-radius: 15px;
+    padding: 0.5rem 1rem;
+    display: grid;
+    * {
+      font-weight: bold;
+    }
+    .QuestName {
+      grid-area: Name;
+    }
+    .QuestRate {
+      grid-area: Progress;
+      font-weight: normal;
+      text-align: start;
+    }
+    .ProgressBar {
+      grid-area: Bar;
+      border-radius: 0.5rem;
+    }
+    .QuestPoint {
+      grid-area: Point;
+      font-size: 1.2rem;
+      justify-self: end;
+      align-self: center;
+    }
+  }
+  .MonthlyQuest {
+    .QuestContainer {
+      width: 100%;
+
+      grid-template-areas:
+        "Name Point"
+        "Bar Point";
+      grid-template-columns: 5fr 2fr;
+      row-gap: 0.5rem;
+      background: linear-gradient(
+        77.32deg,
+        rgba(187, 134, 252, 0.9) 46.71%,
+        rgba(134, 195, 252, 0.9) 98.58%,
+        rgba(232, 234, 255, 0.9) 177.76%
+      );
+
+      * {
+        color: black;
+      }
+      .ProgressBar {
+        color: #2f3037;
+      }
+    }
+  }
+  .DailyQuest {
+    .QuestList {
+      width: 100%;
+      height: 160px;
+      .QuestContainer {
+        width: 150px;
+        height: 150px;
+        margin-right: 1rem;
+
+        grid-template-areas:
+          "Name"
+          "Bar"
+          "Progress"
+          "Point";
+        grid-template-rows: 2fr 1fr 2fr 1fr;
+
+        &:nth-child(odd) {
+          background: linear-gradient(
+            38.38deg,
+            rgba(2, 218, 197, 0.75) -7.52%,
+            rgba(255, 206, 81, 0.75) 165.07%
+          );
+        }
+        &:nth-child(even) {
+          background: linear-gradient(
+            224.34deg,
+            rgba(0, 194, 255, 0.85) 8.03%,
+            rgba(0, 26, 255, 0.3995) 182.75%
+          );
+        }
+
+        .ProgressBar {
+          color: white;
+        }
       }
     }
   }
